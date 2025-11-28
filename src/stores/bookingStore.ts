61ea141a4b8c1ef88e296949d6f97e8f98d16c84
Hanna <hannaaosterberg@hotmail.com>
2025-11-28 10:16:37 +0100

    uncommented store logic

--- src/stores/bookingStore.ts
+++ src/stores/bookingStore.ts
@@ -1,76 +1,76 @@
-import { defineStore } from "pinia";
+// import { defineStore } from "pinia";
 
-export const useBookingStore = defineStore('booking', {
-    state: () => ({
-        experience: null as null | {
-            id: number;
-            title: string;
-            description: string;
-            price: number;
-            image: string;
-            tags: string[];
-            totalExperiencePrice: number;
-        },
-        date: null as string | null,
-        people: 1,
-        ageCategory: 'adult',
+// export const useBookingStore = defineStore('booking', {
+//     state: () => ({
+//         experience: null as null | {
+//             id: number;
+//             title: string;
+//             description: string;
+//             price: number;
+//             image: string;
+//             tags: string[];
+//             totalExperiencePrice: number;
+//         },
+//         date: null as string | null,
+//         people: 1,
+//         ageCategory: 'adult',
 
-        addons: [] as Array<{
-            id: number;
-            title: string;
-            priceType: string;
-            priceValue: any;
-            totalExperiencePrice?: number;
-        }>
-    }),
+//         addons: [] as Array<{
+//             id: number;
+//             title: string;
+//             priceType: string;
+//             priceValue: any;
+//             totalExperiencePrice?: number;
+//         }>
+//     }),
 
-    getters: {
-        totalExperiencePrice: (state) => state.experience
-        ? state.experience.price * state.people
-        : 0,
+//     getters: {
+//         totalExperiencePrice: (state) => state.experience
+//         ? state.experience.price * state.people
+//         : 0,
 
-        addonsTotal: (state) => {
-            let total = 0
+//         addonsTotal: (state) => {
+//             let total = 0
 
-            for (const add of state.addons) {
-                if(add.priceType === 'fixed') {
-                    total += add.priceValue;
-                } else if (add.priceType === 'percentage') {
-                    total += (state.totalExperiencePrice * add.priceValue) / 100
-                } else if (add.priceType === 'range') {
-                    total += add.priceValue.min;
-                }
-            }
+//             for (const add of state.addons) {
+//                 if(add.priceType === 'fixed') {
+//                     total += add.priceValue;
+//                 } else if (add.priceType === 'percentage') {
+//                     total += (state.totalExperiencePrice * add.priceValue) / 100
+//                 } else if (add.priceType === 'range') {
+//                     total += add.priceValue.min;
+//                 }
+//             }
 
-            return total
+//             return total
 
-        },
+//         },
 
-        totalPrice: (state) => {
-            return state.totalExperiencePrice + state.addonsTotal
-        }
-    },
+//         totalPrice: (state) => {
+//             return state.totalExperiencePrice + state.addonsTotal
+//         }
+//     },
 
-    actions: {
-        setExperience(exp) {
-            this.experience = exp;
-        },
-        updatePeople(n: number) {
-            this.people = n;
-        },
-        setDate(date: string) {
-            this.date = date;
-        },
-        setAgeCategory(cat: string) {
-            this.ageCategory = cat;
-        },
-        addAddon(addon) {
-            if(!this.addons.find(a => a.id === addon.id)) {
-                this.addons.push(addon);
-            }
-        },
-        removeAddon(id: number) {
-            this.addons = this.addons.filter(a => a.id !== id);
-        }
-    }
-})
+//     actions: {
+//         setExperience(exp) {
+//             this.experience = exp;
+//         },
+//         updatePeople(n: number) {
+//             this.people = n;
+//         },
+//         setDate(date: string) {
+//             this.date = date;
+//         },
+//         setAgeCategory(cat: string) {
+//             this.ageCategory = cat;
+//         },
+//         addAddon(addon) {
+//             if(!this.addons.find(a => a.id === addon.id)) {
+//                 this.addons.push(addon);
+//             }
+//         },
+//         removeAddon(id: number) {
+//             this.addons = this.addons.filter(a => a.id !== id);
+//         }
+//     }
+// })
