c05269107b50549ba53b9dfad1eae65726e6504a
stalstierna <andrea.stalstierna@chasacademy.se>
2025-11-29 15:53:37 +0100

    details-fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import CartPage from "../pages/CartPage.vue";
 import LandingPage from "../pages/LandingPage.vue";
+import BookingPage from "../pages/BookingPage.vue";
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", component: LandingPage },
     { path: "/cart", component: CartPage },
-    { path: "/booking/:id", name: "booking" },
+    //  TODO :/id på booking sen
+    { path: "/booking", name: "booking", component: BookingPage },
     { path: "/experiences", name: "experiences" },
     { path: "/experience/:id", name: "experience" },
   ],
